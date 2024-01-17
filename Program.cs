55fc48464f7b1910ef91dadfbfcd782e6485a127
bbundy12 <brendanbundy1@gmail.com>
2024-01-17 16:58:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using DiceRollSimulator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Create an instance of the diceRole class to simulate dice rolls.
+        diceRole dr = new diceRole();
+
+        // Greet the user and ask for the number of dice rolls to simulate.
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\n" +
+            "How many dice rolls would you like to simulate?");
+
+        int num = int.Parse(System.Console.ReadLine());
+
+        // Store the results in an array from the diceRoleSimulations method
+        int[] rolls = dr.diceRoleSimulation(num);
+
+        // Display the header for the simulation results.
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\n" +
+                                "Each \"*\" represents 1 % of the total number of rolls.\n" +
+                                "Total number of rolls = " + num + ".\n");
+
+        // Initialize an empty string for the output.
+        string output = "";
+
+        // Loop through the possible dice numbers (2 to 12).
+        for (int diceNum = 2; diceNum < 13; diceNum++)
+        {
+            // Calculate the percentage of each dice number.
+            decimal percentage = (decimal)rolls[diceNum] / num * 100;
+            int roundedPercentage = (int)Math.Round(percentage);
+
+            // Count the total rolls for this dice number.
+            int totalRolls = rolls[diceNum];
+
+            // Prepare the line for this dice number in the output.
+            output += diceNum + ": ";
+
+            // Add asterisks representing the percentage.
+            for (int asterisk = 0; asterisk < percentage; asterisk++)
+            {
+                output += '*';
+            }
+            output += "\n";
+        }
+
+        // Display the final output and a closing message.
+        System.Console.Write(output + "\nThank you for using the dice throwing simulator. Goodbye!\n");
+    }
+}
---
+++ diceRole.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollSimulator
+{
+    internal class diceRole
+    {
+        // Random number generator for dice rolls.
+        Random rnd = new Random();
+
+        // Method to simulate dice rolls.
+        public int[] diceRoleSimulation(int num)
+        {
+            // Initialize variables for the two dice and the total of their roll.
+            int dice1 = 0;
+            int dice2 = 0;
+            int rollTotal = 0;
+
+            // Create an array to store the count of each possible roll total (2-12).
+            int[] rolls = new int[13]; 
+
+            // Loop through the number of rolls specified.
+            for (int roll = 0; roll < num; roll++)
+            {
+                // Roll two dice.
+                dice1 = rnd.Next(1, 7);
+                dice2 = rnd.Next(1, 7);
+
+                // Sum the values of the two dice.
+                rollTotal = dice1 + dice2;
+
+                // Increment the count for this roll total.
+                rolls[rollTotal]++;
+            }
+
+            // Return the array containing the counts of each roll total.
+            return rolls;
+        }
+    }
+}
